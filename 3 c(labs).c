ea6399c83105bae2169f4a97e8e06edba7ababeb
Isira Adithya Withana <135606461+IsiraAdithyaWithana@users.noreply.github.com>
2023-06-05 16:11:19 +0530

    Lab 03 Q 05

---
+++ 3 c(labs).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2,n;
+
+    printf("Enter two integers\n");
+    scanf("%d %d",&n1,&n2);
+
+    n=n1%n2;
+
+    if(n==0)
+    {
+        printf("%d is a multiple of %d",n1,n2);
+    }
+    else
+    {
+        printf("%d is not a multiple of %d",n1,n2);
+    }
+}
